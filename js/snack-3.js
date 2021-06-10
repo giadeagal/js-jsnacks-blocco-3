c3a9f544d3dd605d94ae8d00ce4b501790a7f8f0
Gianluca <gl.galassini@gmail.com>
2021-06-10 13:43:04 +0200

    snack 3

---
+++ js/snack-3.js
@@ -0,0 +1,18 @@
+// Scrivi una funzione che fonda due array (con lo stesso numero di elementi) prendendo alternativamente gli elementi da uno e dall’altroes. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+var listOne = [1, 2, 3];
+var listTwo = ["a", "b", "c"];
+
+function merge(x, y) {
+    if (x.length != y.length) {
+        alert("I due array devono avere la stessa lunghezza")
+    };
+    var elenco = [];
+
+    for (var i = 0; i < x.length; i++) {
+        elenco.push(x[i], y[i]);
+    };
+    return elenco;
+};
+
+document.getElementById("printer").innerHTML = merge(listOne, listTwo);
