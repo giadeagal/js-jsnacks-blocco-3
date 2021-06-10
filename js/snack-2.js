1c420eab25dc2a9b8b4309a363bb05b75b418d4a
Gianluca <gl.galassini@gmail.com>
2021-06-10 13:32:44 +0200

    snack 2

---
+++ js/snack-2.js
@@ -0,0 +1,22 @@
+// Dare la possibilità di inserire due parole. 
+// Verificare tramite una funzione che le due parole abbiano la stessa lunghezza.
+// Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+var wordOne = prompt("inserisci una parola");
+var wordTwo = prompt("inserisci una parola");
+
+function sameLen(x, y) {
+    var controllo = 0;
+    if (x.length == y.length) {
+        controllo = 1;
+    }
+    return controllo;
+}
+
+if (sameLen(wordOne, wordTwo)) {
+    document.getElementById("printer").innerHTML=wordOne + " " + wordTwo;
+} else if(!sameLen(wordOne, wordTwo) && wordOne.length<wordTwo.length) {
+    document.getElementById("printer").innerHTML=wordTwo;
+} else {
+    document.getElementById("printer").innerHTML=wordOne;
+};
